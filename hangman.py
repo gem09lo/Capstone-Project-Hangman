4db22aea10460c25a6e60b65e9a8ef72e588b688
Your Name <gemmaw09lo@gmail.com>
2024-08-17 21:30:07 +0100

    added total_correct_letters list to store correct letters from all guesses

--- hangman.py
+++ hangman.py
@@ -15,13 +15,14 @@ def check_guess(current_guess, word):
         for j in word:
             if i == j and i not in correct_letters:
                 correct_letters.append(i)
-    print ("Correct letters: {}".format(correct_letters))
+    return (correct_letters)
 
 
 
 def game():
     word = secret_word()
     guessed_letters = set()
+    total_correct_letters = []
 
     for guess_num in range(1,4):
         guess = user_guess()
@@ -32,6 +33,9 @@ def game():
             break
         else:
             correct_letters = check_guess(guess, word)
+            total_correct_letters.extend(correct_letters)
+            print("Correct letters: {}".format(total_correct_letters))
+
             guessed_letters.update(list(guess))
             print("Guessed letters: {}".format(guessed_letters))
             print("Guess again \n")
